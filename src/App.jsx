b5b03817c447ab20286dc61879d0971b977e04dd
Dylan Abbett <abbettdylan@gmail.com>
2024-08-05 17:51:40 -0400

    reformat

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-
 export default function App() {
   return (
-  <form className="bg-slate-200 h-screen px-8 py-8 text-3xl">
-    <div className="">
-      <label htmlFor=""> New Item</label>
-      <input type="text" id="item"/>
-    </div>
-    <button className="bg-slate-600 rounded-full p-3"> Submit </button>
-  </form>
-  )
+    <>
+      <form className="bg-slate-200 h-screen px-8 py-8 text-3xl">
+        <div className="">
+          <label htmlFor=""> New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="bg-slate-600 rounded-full p-3"> Submit </button>
+      </form>
+
+      <h1 className=""> Todo List</h1>
+    </>
+  );
 }
