e68d0e4bf92eebc6ce78c6e5e261018cd13f6924
Dylan Abbett <abbettdylan@gmail.com>
2024-08-05 17:56:58 -0400

    adjust tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 export default function App() {
   return (
-    <>
-      <form className="bg-slate-200 h-screen px-8 py-8 text-3xl">
-        <div className="">
-          <label htmlFor=""> New Item</label>
-          <input type="text" id="item" />
-        </div>
-        <button className="bg-slate-600 rounded-full p-3"> Submit </button>
-      </form>
+    <div className="bg-slate-200 h-screen px-8 py-8">
+       <form className=" text-3xl">
+    <div className="">
+      <label htmlFor=""> New Item</label>
+      <input type="text" id="item" />
+    </div>
+    <button className="bg-slate-600 rounded-full p-3"> Submit </button>
+  </form>
 
-      <h1 className=""> Todo List</h1>
-    </>
+  <h1 className="text-2xl py-6"> Todo List</h1>
+ </div>
+      
   );
 }
