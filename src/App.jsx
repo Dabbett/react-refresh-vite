d8a2e70117c179b0d8b3be442c813127d41da998
Dylan Abbett <abbettdylan@gmail.com>
2024-08-06 16:38:24 -0400

    update app.js to create delete and handle button clicks

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,45 @@ import { useState } from "react";
 
 export default function App() {
   const [newItem, setNewItem] = useState("");
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     setTodos((currentTodos) => {
       return [
         ...currentTodos,
-         { id: crypto.randomUUID(), title: newItem, completed: false},
-        ]
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter(todo => {
+        return todo.id !== id
     })
-
-}
-
-
-
+    })
+  }
 
   return (
     <div className="bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300 h-screen px-8 py-8 flex flex-col items-center ">
-      <form onSubmit={handleSubmit} className="bg-gray-400 p-6 rounded-lg shadow-md w-full max-w-md">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-gray-400 p-6 rounded-lg shadow-md w-full max-w-md"
+      >
         <div className="mb-4">
           <label
             htmlFor="item"
@@ -45,21 +64,27 @@ export default function App() {
       <h1 className="text-2xl py-6 text-gray-800 font-bold">Todo List</h1>
 
       <ul className="w-full max-w-md">
-        {todos.map(todo => {
-          return(
-            <li key={todo.id} className="flex items-center justify-between bg-white p-4 rounded-lg shadow-md mb-4">
+        {todos.length === 0 && 'No Tasks'}
+        {todos.map((todo) => {
+          return (
+            <li
+              key={todo.id}
+              className="flex items-center justify-between bg-white p-2 rounded-lg shadow-md mb-2"
+            >
               <label htmlFor="item2" className="text-gray-900">
-                <input type="checkbox" checked={todo.completed} />
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
                 {todo.title}
               </label>
-            <button className="border-solid border-2 rounded-xl border-slate-600 text-slate-600 p-1 ml-2 hover:bg-slate-600 hover:text-white">
-              Delete
-            </button>
-          </li>
-          )
+              <button onClick={() => deleteTodo(todo.id)} className="border-solid border-2 rounded-xl border-slate-600 text-slate-600 p-1 ml-2 hover:bg-slate-600 hover:text-white">
+                Delete
+              </button>
+            </li>
+          );
         })}
-        
-        
       </ul>
     </div>
   );
